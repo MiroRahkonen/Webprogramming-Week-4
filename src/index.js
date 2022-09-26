644ff1dde9942e7678cef041c16ca9fc9a9e75fb
MiroRahkonen <80097068+MiroRahkonen@users.noreply.github.com>
2022-09-26 08:05:43 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 const submitButton = document.getElementById("submit-data");
 const list = document.getElementById("showlist");
 
-submitButton.addEventListener("click", search, false);
+submitButton.addEventListener("click",search,false);
 
-async function search(event) {
-  list.innerHTML = "";
-  event.preventDefault();
-  let searchterm = document.getElementById("input-show").value;
-  let url = "https://api.tvmaze.com/search/shows?q=" + searchterm;
-  let promise = await fetch(url);
-  let dataJSON = await promise.json();
-  let stringJSON = JSON.stringify(dataJSON);
-  let json = JSON.parse(stringJSON);
-  let length = json.length;
-  for (let i = 0; i < length; i++) {
-    let imgsrc = "";
-    if (json[i].show.image != null){/*checking if there is an image src*/
-      imgsrc = Object.values(json[i].show.image)[0];
+async function search(event){
+    list.innerHTML = "";
+    event.preventDefault();
+    let searchterm = document.getElementById("input-show").value;
+    let url = "https://api.tvmaze.com/search/shows?q=" +searchterm
+    let promise = await fetch(url);
+    let dataJSON = await promise.json();
+    let stringJSON = JSON.stringify(dataJSON);
+    let json = JSON.parse(stringJSON);
+    let length = json.length;
+    for(let i=0;i<length;i++){
+        let imgsrc = "";
+        if((json[i].show.image) != null){   /*checking if there is an image src*/ 
+            imgsrc = Object.values(json[i].show.image)[0];
+        }
+        let title = json[i].show.name;
+        let summary = json[i].show.summary;
+        list.innerHTML += '<div class="show-data"><img src="'+imgsrc+'"> <div class="show-info"><h1>'+title+'</h1>'+summary+'</div></div>'
     }
-    let title = json[i].show.name;
-    let summary = json[i].show.summary;
-    list.innerHTML += '<div class="show-data"><img src="'+imgsrc+'"><div class="show-info"><h1>'+title+"</h1>"+summary+"</div></div>";
-  }
 }
